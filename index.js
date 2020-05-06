a3c3c4a2f6337e6f98c888f3dd26ebbbdecb6c9a
Mai Vo <mvo36@gatech.edu>
2020-05-06 16:45:02 -0400

    experience done, working on contact

---
+++ index.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', "./icons/linkedin_mars.JPG");
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', "./icons/linkedin_new.jpg");
+  }
